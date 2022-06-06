02712b4bf252c10170dbcbb35d5145ac5a546942
xiayue1 <applied.energetic@gmail.com>
2022-06-06 11:08:54 +0800

    feat:将邮件发送功能写进util库

--- django-vue-admin-main/backend/dvadmin/utils/mail.py
+++ django-vue-admin-main/backend/dvadmin/utils/mail.py
@@ -10,11 +10,12 @@ from django.core.mail import send_mail
 from application import settings
 from django.shortcuts import HttpResponse
 from django.core.mail import send_mail
-
-def send_email_demo(request):
+#传入接收者的列表即可
+#recipient_list: 一个字符串列表，每项都是一个邮箱地址。
+def send_email_demo(target):
     from_who = settings.EMAIL_FROM  
     #发件人  已写在 配置中 可直接使用（163邮箱）
-    to_who = '631526820@qq.com' 
+    to_who = target 
     #收件人 是一个列表 不限邮箱
     subject = '检测到未出勤情况，请尽快回复'  
     # 发送的标题
@@ -26,14 +27,14 @@ def send_email_demo(request):
 
 #python manage.py shell
 
-#进入Django Shell 使用电子邮件
-from django.core.mail import send_mail
-from application import settings
-send_mail(
-    subject='检测到未出勤情况，请尽快回复',
-    message='检测到未出勤情况，请尽快回复',
-    from_email=settings.EMAIL_HOST_USER,
-    recipient_list=['631526820@qq.com'])
+# #进入Django Shell 使用电子邮件
+# from django.core.mail import send_mail
+# from application import settings
+# send_mail(
+#     subject='检测到未出勤情况，请尽快回复',
+#     message='检测到未出勤情况，请尽快回复',
+#     from_email=settings.EMAIL_HOST_USER,
+#     recipient_list=['631526820@qq.com'])
 
 """
 源码: send_mail 的源码 里面参数很多
